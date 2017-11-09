49262ab03af895b8b5338da0901356b556c2870e
Jeff Lindsay <progrium@gmail.com>
2017-11-09 13:45:37 -0600

    basic docs for top level package
    Signed-off-by: Jeff Lindsay <progrium@gmail.com>

--- com.go
+++ com.go
@@ -1,12 +1,37 @@
+// Package com is the user-facing interface to the object registry. Since
+// the only part of the API you need to use is Register, the rest of the API
+// for interacting with an object registry is in its own package. This is used
+// by other tooling building around com, for example the config subpackage.
+//
+// When you register an object, it will populate fields based on the com struct
+// tags used on them. The object will then also be used to populate fields
+// of other objects in the registry where the type or interface matches.
+//
+//  type Component struct {
+//  	Log      log.Logger     `com:"singleton"`
+//  	Handlers []api.Handlers `com:"extpoint"`
+//  	DB       api.Store      `com:"config"`
+//  }
+//
+// In the above example component, it has fields with all three possible struct
+// tags. Singleton will pick the first object in the registry that implements
+// that interface. You can also use pointers to concrete types, for example to
+// other components. Extpoint is going to be a slice of all objects in the
+// registry that implement that interface. Config is not populated, but is allowed
+// to be populated via the registry API. If you're using the config package, it
+// will do this for you and populate it based on configuration. In this case,
+// the key would be "DB" and the value could be the name of any registered
+// component that implements api.Store.
 package com
 
 import "github.com/gliderlabs/com/objects"
 
 var (
-	// DefaultRegistry is often used as the single top level registry for an app
+	// DefaultRegistry is often used as the single top level registry for an app.
 	DefaultRegistry = &objects.Registry{}
 )
 
+// Register will add an object with optional name to the default registry.
 func Register(obj interface{}, name string) error {
 	return DefaultRegistry.Register(objects.New(obj, name))
 }
